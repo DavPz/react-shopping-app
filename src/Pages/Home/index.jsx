3c30e63b96c9d5cb0b858395815728c745eaf8a9
David.Pugliese <davplzt@gmail.com>
2024-07-11 22:24:17 -0500

    :pencil:  API Implementation and Layaout cards
    Implementation of fake store API and making a layaout for all cards.

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../../Components/Layout';
 import Card from '../../Components/Card';
 
 function Home() {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, []);
+
+  useEffect(() => {
+    console.log(items);
+  }, [items]);
+
   return (
     <Layout>
       Home
-      <Card />
+      <div className="grid grid-cols-4 gap-4 w-full max-w-screen-lg">
+        {
+          items.map((item) => (
+            <Card key={item.id} item={item} />
+          ))
+
+        }
+
+      </div>
     </Layout>
   );
 }
